7c5195365d562895541bc0e1e0df22a588d3b1f4
NovaFlash321 <novaflash321@gmail.com>
2024-02-04 01:59:04 -0500

    Designed the application to be more accessible and user friendly

--- Password Generator/Form1.Designer.cs
+++ Password Generator/Form1.Designer.cs
@@ -28,49 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            richTextBox1 = new RichTextBox();
+            buttonPassword = new Button();
+            passwordLength = new TrackBar();
+            label1 = new Label();
+            passwordLengthDisplay = new Label();
+            textBoxPassword = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)passwordLength).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // buttonPassword
             // 
-            button1.Font = new Font("Olivers Barney", 11.25F, FontStyle.Italic, GraphicsUnit.Point);
-            button1.Location = new Point(226, 189);
-            button1.Name = "button1";
-            button1.Size = new Size(201, 96);
-            button1.TabIndex = 0;
-            button1.Text = "Click to Generate Password";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            buttonPassword.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            buttonPassword.Font = new Font("Olivers Barney", 11.25F, FontStyle.Italic, GraphicsUnit.Point);
+            buttonPassword.Location = new Point(12, 363);
+            buttonPassword.Name = "buttonPassword";
+            buttonPassword.Size = new Size(765, 96);
+            buttonPassword.TabIndex = 0;
+            buttonPassword.Text = "Click to Generate Password";
+            buttonPassword.UseVisualStyleBackColor = true;
+            buttonPassword.Click += button1_Click;
             // 
-            // richTextBox1
+            // passwordLength
             // 
-            richTextBox1.BorderStyle = BorderStyle.FixedSingle;
-            richTextBox1.Font = new Font("Olivers Barney", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            richTextBox1.Location = new Point(12, 12);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.ReadOnly = true;
-            richTextBox1.ScrollBars = RichTextBoxScrollBars.None;
-            richTextBox1.Size = new Size(620, 65);
-            richTextBox1.TabIndex = 2;
-            richTextBox1.Text = "Password Generator\n";
+            passwordLength.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            passwordLength.Location = new Point(12, 70);
+            passwordLength.Maximum = 25;
+            passwordLength.Minimum = 10;
+            passwordLength.Name = "passwordLength";
+            passwordLength.Orientation = Orientation.Vertical;
+            passwordLength.Size = new Size(45, 287);
+            passwordLength.TabIndex = 3;
+            passwordLength.TickStyle = TickStyle.Both;
+            passwordLength.Value = 10;
+            passwordLength.Scroll += trackBar1_Scroll;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Olivers Barney", 36F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(623, 58);
+            label1.TabIndex = 4;
+            label1.Text = "Password Generator\n";
+            // 
+            // passwordLengthDisplay
+            // 
+            passwordLengthDisplay.AutoSize = true;
+            passwordLengthDisplay.Font = new Font("Olivers Barney", 47.9999924F, FontStyle.Regular, GraphicsUnit.Point);
+            passwordLengthDisplay.Location = new Point(63, 79);
+            passwordLengthDisplay.Name = "passwordLengthDisplay";
+            passwordLengthDisplay.Size = new Size(159, 77);
+            passwordLengthDisplay.TabIndex = 6;
+            passwordLengthDisplay.Text = "???";
+            passwordLengthDisplay.Click += label2_Click;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxPassword.BackColor = SystemColors.Menu;
+            textBoxPassword.BorderStyle = BorderStyle.FixedSingle;
+            textBoxPassword.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBoxPassword.Location = new Point(63, 300);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(714, 57);
+            textBoxPassword.TabIndex = 7;
+            textBoxPassword.Text = "??????????????";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonShadow;
-            ClientSize = new Size(647, 297);
-            Controls.Add(richTextBox1);
-            Controls.Add(button1);
+            ClientSize = new Size(789, 471);
+            Controls.Add(textBoxPassword);
+            Controls.Add(passwordLengthDisplay);
+            Controls.Add(label1);
+            Controls.Add(passwordLength);
+            Controls.Add(buttonPassword);
             Name = "Form1";
             Text = "Password Generator";
+            ((System.ComponentModel.ISupportInitialize)passwordLength).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private RichTextBox richTextBox1;
+        private Button buttonPassword;
+        private TrackBar passwordLength;
+        private Label label1;
+        private Label passwordLengthDisplay;
+        private RichTextBox textBoxPassword;
     }
 }
--- Password Generator/Form1.cs
+++ Password Generator/Form1.cs
@@ -5,9 +5,11 @@ namespace Password_Generator
 
 
         #region Variables
-
-
-
+        int[] asciiCharacters = {33,34,35,36,37,38,39,40,41,42,43,45,46,47,48,49,
+            50,51,52,53,54,55,56,57,58,63,64,65,66,67,68,69,70,71,72,73,74,75,76,
+            77,78,79,80,81,82,83,84,85,85,87,88,89,90,91,92,93,97,98,99,100,101,
+            102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122};
+        Random _rnGesus = new Random();
         #endregion
 
 
@@ -20,16 +22,39 @@ namespace Password_Generator
         public Form1()
         {
             InitializeComponent();
+            passwordLengthDisplay.Text = passwordLength.Value.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            textBoxPassword.Text = SetPassword(passwordLength.Value);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            passwordLengthDisplay.Text = passwordLength.Value.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string SetPassword(int passwordLength)
+        {
+            string _password = "";
+            for (int i = 0; i < passwordLength; i++)
+            {
+                _password += (char)asciiCharacters[_rnGesus.Next(0, asciiCharacters.Length)];
+            }
+
+            return _password;
+        }
+
     }
 }
